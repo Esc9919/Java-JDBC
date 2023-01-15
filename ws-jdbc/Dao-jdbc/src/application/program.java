3b10d39fbf683eb874b6d910af1025b0e3117196
Esc9919 <Emersoncaldeira1999@hotmail.com>
2023-01-15 02:07:59 -0300

    Criando a classe entidade departamento

---
+++ ws-jdbc/Dao-jdbc/src/application/program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Departiment;
+
+public class program {
+
+	public static void main(String[] args) {
+
+		Departiment obj = new Departiment(1, "Books");
+		System.out.println(obj);
+		
+	}
+
+}
